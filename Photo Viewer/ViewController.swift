9a162d637242665bb0d1c03025acff6d0be345d8
Maciej Matuszewski <maciej.matuszewski@netguru.pl>
2018-03-13 18:52:29 +0100

    Initial Commit

---
+++ Photo Viewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Photo Viewer
+//
+//  Created by Maciej Matuszewski on 13.03.2018.
+//  Copyright © 2018 Maciej Matuszewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
